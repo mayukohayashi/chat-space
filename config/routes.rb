a60ec9b863c2e6e93fa523ed79d30ea05e6b6839
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-09-26 15:34:34 +0900

    create temporary message page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'messages#index'
   get 'messages/new' => 'messages#new'
   get 'messages/edit' => 'messages#edit'
+  get 'messages/message' => 'messages#message'
 end
