6847be1e03b62e4012d440662dd3b771a539eafa
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-09-29 00:27:34 +0900

    create　.gitignore

--- app/models/group.rb
+++
@@ -1,5 +0,0 @@
-class Group < ApplicationRecord
-  has_many :group_users
-  has_many :users, through: :group_users
-  validates :name, presence: true, uniqueness: true
-end
--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  devise_for :users
-  root 'messages#index'
-  resources :users, only: [:index, :edit, :update]
-  resources :groups, only: [:index, :new, :create, :edit, :update] do
-    resources :messages, only: [:index, :create]
-  end
-end
