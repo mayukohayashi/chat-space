592067b25c91d8927d67b1a942f6bfaaf6e81fc0
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-09-29 00:28:38 +0900

    delete caches

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,30 @@
+class GroupsController < ApplicationController
+  def index
+  end
+
+  def new
+    @group = Group.new
+    @group.users << current_user
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+# TODO/chenge rocket?????
+  private
+    def group_params
+      params.require(:group).permit(:name, { :user_ids => [] })
+    end
+end
