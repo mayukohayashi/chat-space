cd675ff1e0d90414a5eecd20705edd1630002b59
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-09-28 21:05:07 +0900

    add index in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
   resources :users, only: [:index, :edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update] do
+  resources :groups, only: [:index, :new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
 end
