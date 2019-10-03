4f3c15bb4bee93a3463401f39cc2aab8abda8b6c
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-10-03 17:24:35 +0900

    messaage =>messages

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,6 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
+
   def index
   end
 
@@ -21,17 +23,20 @@ class GroupsController < ApplicationController
 
   def update
     if @group.update(group_params)
-      redirect_to group_messaages_path(@group), notice: 'グループを編集しました'
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
     else
       render :edit
     end
   end
 
   private
+
+    def group_params
+      params.require(:group).permit(:name, user_ids: [])
+    end
+
     def set_group
       @group = Group.find(params[:id])
     end
-    def group_params
-      params.require(:group).permit(:name, { :user_ids => [] })
-    end
-end
+
+  end
