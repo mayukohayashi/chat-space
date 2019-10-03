0f9204c7dc534f62010a0387df931e44dfca654b
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-10-03 10:09:10 +0900

    memo

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -6,3 +6,10 @@ $(document).on('turbolinks:load', function(){
   });
 
 });
+
+// $(document).on('turbolinks:load', function(){ //リロードしなくてもjsが動くようにする
+//   $(document).on('keyup', '#form', function(e){ //このアプリケーション(document)の、formというid('#form')で、キーボードが押され指が離れた瞬間(.on('keyup'...))、eという引数を取って以下のことをしなさい(function(e))
+//     e.preventDefault(); //キャンセル可能なイベントをキャンセル
+//     var input = $.trim($(this).val()); //この要素に入力された語句を取得し($(this).val())、前後の不要な空白を取り除いた($.trim(...);)上でinputという変数に(var input =)代入
+//   });
+// });
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,13 @@ class UsersController < ApplicationController
   def index
   end
 
+  # def index
+  #   @users = User.where('name LIKE(?)', "%#{params[:name]}%" ).where.not(id: current_user)
+  #   respond_to do |format|
+  #     format.json
+  #   end
+  # end
+
   def edit
   end
 
