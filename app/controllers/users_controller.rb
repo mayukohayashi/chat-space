7cea579323a5726586b35b8ebc0c2ce41628940f
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-10-03 16:36:09 +0900

    can geet name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
   def index
-    @users = User.where ('name LIKE(?)', "%#{params[:keyword]}%")
-    render json: @users
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(id: params[:group_users_id])
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
   def edit
@@ -14,8 +17,6 @@ class UsersController < ApplicationController
       render :edit
     end
   end
-
-  private
 
   def user_params
     params.require(:user).permit(:name, :email)
