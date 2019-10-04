1091e9ec429fa43cbb9dd89d7b32e9ba4824bf53
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-10-04 21:58:30 +0900

    make api routes

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,4 +1,8 @@
 class Api::MessagesController < ApplicationController
   def index
+    respond_to do |format|
+      format.html
+      format.json { @new_messages = Message.where("group_id = ? AND id > ?", params[:group], params[:id]) }
+    end
   end
 end
