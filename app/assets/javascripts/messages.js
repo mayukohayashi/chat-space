4f39e869e09ad9ac462d9cf05d765a92b7bfa196
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-10-04 22:51:45 +0900

    this to make

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -36,12 +36,6 @@ $(document).on('turbolinks:load', function(){
       })
     .done(function(data){
 
-      // function scrollBottom(){
-      //   var target = $('.messages').last();
-      //   var position = target.offset().top + $('.chat-main__messages').scrollTop();
-      //   $('.chat-main__messages').animate({scrollTop: position}, 1000, 'swing');
-      // }
-
         var html = buildHTML(data);
         $('.chat-main__messages').append(html);
         $('#message_content').val('');
@@ -55,4 +49,32 @@ $(document).on('turbolinks:load', function(){
         $('.chat-main__form__new-message__submit-btn').prop('disabled', false);
       })
     })
+
+
+    $(function() {
+
+        var reloadMessages = function() {
+          //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+          last_message_id = ※※※
+          $.ajax({
+            //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+            url: ※※※,
+            //ルーティングで設定した通りhttpメソッドをgetに指定
+            type: 'get',
+            dataType: 'json',
+            //dataオプションでリクエストに値を含める
+            data: {id: last_message_id}
+          })
+          .done(function(messages) {
+            console.log('success');
+          })
+          .fail(function() {
+            console.log('error');
+          });
+        };
+      });
+
+
+
+
   })
